5d756b3fac7e0e81662c8889267584c9f63547ff
Max Gautier <mgautier@student.42.fr>
2016-12-06 18:29:23 +0100

    Add header
    Add struct to keep the file cache (buffer for each file)
    Add struct to keep each line for one file (store in the file cache)

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/06 18:23:03 by mgautier          #+#    #+#             */
+/*   Updated: 2016/12/06 18:27:53 by mgautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+typedef struct	s_line
+{
+	char	*str;
+	t_bool	is_complete;
+}				t_line;
+
+typedef struct	s_file_cache
+{
+	t_list	*lines;
+	t_bool	file_is_over;
+}				t_file_cache;
+
+#endif
