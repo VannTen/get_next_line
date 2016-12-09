21e63d1b748ceb60e333353cb242ad9931e130c0
Max Gautier <mgautier@student.42.fr>
2016-12-09 18:37:37 +0100

    Move part of read_file in read_cache
    Shortens read file, which now only has to deal with reading from the fd
    and appends what is read to its first parameter. But read_cache is too
    long.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 10:36:07 by mgautier          #+#    #+#             */
-/*   Updated: 2016/12/07 14:32:09 by mgautier         ###   ########.fr       */
+/*   Updated: 2016/12/09 18:26:19 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,39 +64,67 @@ t_file_cache	ft_cache_file(const int fd, const t_database db)
 }
 
 /*
-** ft_read_file
+** ft_read_cache
 **
-** This function populate the file_cache with the next line, calling itself
-** recursively until is has at least one complete line (aka, one finished
-** with a newline or EOF
-** It creates a new line from the previous incomplete one, until either the file
-** is finished, or a newline character is found.
+** This function reads a file cache, and put the next line of it in str ;
+** if necessary, it reads from the associated file, using ft_read_file.
 */
 
-t_file_cache	*ft_read_file(t_file_cache *file_cache)
+int				ft_read_cache(t_file_cache file, char **str)
 {
-	char	*incomplete_line;
-	char	*complete_line;
-	char	buf[BUF_SIZE + 1];
+	t_lst	*next_link;
+	char	*str;
+	int		read_result;
 
-	incomplete_line = (char*)file_cache->lines->content;
-	oct_read = read(file_cache->fd, &buf, BUF_SIZE);
-	if (oct_read == -1)
-		return (NULL);
-	buf[oct_read] = '\0';
-	complete_line = ft_strtjoin(incomplete_line, buf,
-			ft_strlen(incomplete_line) + oct_read);
-	ft_strdel(incomplete_line);
-	free(file_cache->lines);
-	file_cache->lines = ft_strsplit_list(complete_line, '\n');
-	if (file_cache->lines->next != NULL || oct_read != BUF_SIZE)
+	if (file->lines == NULL)
 	{
-		if (oct_read != BUF_SIZE)
-			file_cache->file_is_over = TRUE;
-		return (file_cache);
+		if (file->is_over)
+			return (FILE_IS_OVER);
+		else
+			file->lines = ft_lstnew_nocpy(NULL);
 	}
-	else
-		return (ft_read_file(file_cache));
+	while (file->lines->next == NULL && file->is_over == FALSE)
+	{
+		str = (char*)lines->content;
+		free(lines);
+		lines = NULL;
+		read_result = ft_read_file(&str, file->fd);
+		if (read_result == FILE_IS_OVER)
+			file->is_over = TRUE;
+		else if (read_result == READ_ERROR)
+			return (READ_ERROR);
+		lines = ft_strplit_lst(str);
+	}
+	*str = (char*)file->lines->content;
+	next_link = file->lines->next;
+	free(file->lines);
+	file->lines = next_link;
+	return (ONE_LINE_READ);
+}
+
+
+/*
+** ft_read_file
+**
+** This function take an incomplete line, read from the file descriptor, and
+** appends which was read to the end on the incomplete line, by joining it with
+** the buffer and replacing it by mean of a pointer.
+*/
+
+int				*ft_read_file(char **line_to_complete, int fd)
+{
+	char	buf[BUF_SIZE + 1];
+	char	*completed_line;
+
+	oct_read = read(fd, &buf, BUF_SIZE);
+	if (oct_read == READ_ERROR)
+		return (READ_ERROR);
+	buf[oct_read] = '\0';
+	completed_line = ft_strtjoin(*line_to_complete, buf,
+			ft_strlen(*line_to_complete) + oct_read);
+	ft_strdel(*line_to_complete);
+	*line_to_complete = completed_line;
+	return (oct_read);
 }
 
 t_file_cache	ft_store_cache(const int fd, const t_database db,
