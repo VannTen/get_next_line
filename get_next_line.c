d84a157a04d823c58d6965150ae06649d1b457f2
Max Gautier <mgautier@student.42.fr>
2016-12-09 18:52:24 +0100

    Suppress cache_file
    The function was an ungly aggregate of reading teh file and creating the
    cache for it.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 10:36:07 by mgautier          #+#    #+#             */
-/*   Updated: 2016/12/09 18:49:56 by mgautier         ###   ########.fr       */
+/*   Updated: 2016/12/09 18:52:19 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,41 +26,6 @@ int				get_next_line(const int fd, char **line)
 	*line = ft_strdup(next_line);
 	ft_strdel(next_line);
 	return (reading_result);
-}
-
-/*
-** ft_cache_file
-**
-** This function read from a file descriptor (usigne the syscall 'read'
-** until is has at least one valid line (depending on BUF_SIZE and the lenght
-** of the line in the file, that can take several calls.
-** If, on the contrary, multiples lines are read, they are split into a string
-** array.
-*/
-
-t_file_cache	ft_cache_file(const int fd, const t_database db)
-{
-	char			buffer[BUF_SIZE + 1];
-	int				oct_read;
-	char			**lines;
-
-	oct_read = 1;
-	buffer[0] = '\0';
-	lines = (char**)ft_memalloc(sizeof(char*) * 2);
-	lines[0] = NULL;
-	lines[1] = NULL;
-	while ((buffer[oct_read - 1] != '\n' || oct_read == BUF_SIZE)
-			&& lines[1] == NULL)
-	{
-		ft_tabdel_not_content(lines);
-		oct_read = read(fd, &buffer, BUF_SIZE);
-		if (oct_read == -1)
-			return (NULL);
-		buffer[oct_read] = '\0';
-		lines = ft_strplit_empty(buffer, '\n');
-		lines[0] = ft_strjoin(incomplete_line, lines[0]);
-	}
-	return (ft_store_cache(fd, db, incomplete_line));
 }
 
 /*
