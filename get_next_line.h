3fef5ceb85c11ee08a1aae8d378c7f4823a72527
Max Gautier <mgautier@student.42.fr>
2016-12-12 13:59:47 +0100

    add constants to header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,22 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 18:23:03 by mgautier          #+#    #+#             */
-/*   Updated: 2016/12/07 13:50:27 by mgautier         ###   ########.fr       */
+/*   Updated: 2016/12/12 13:59:28 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+/*
+** Constants definitions
+*/
+
+# define ONE_LINE_READ 1
+# define READ_ERROR -1
+# define FILE_IS_OVER 0
+# define LINE_DELIMITER '\n'
+# define BUF_SIZE 10
 
 typedef struct	s_line
 {
